8abf93c7f917b87ed1f4322b020e581a515419be
Дмитрий Юраков <dyurakov@mobiledep.ru>
2023-04-11 14:49:21 +0300

    Выполнил 8-11

--- UNIT_1/homework/task11.py
+++ UNIT_1/homework/task11.py
@@ -1,2 +1,15 @@
 #todo: Дан целочисленный массив размера N из 10 элементов.
 #Преобразовать массив, увеличить каждый его элемент на единицу.
+
+import array as arr
+list = [1,2,3,4,5,6,7,8,9,0]
+
+
+massiv = arr.array('i',list)
+
+print(massiv)
+
+
+for i in range(len(massiv)):
+    massiv[i] = massiv[i]+1
+print(massiv)
