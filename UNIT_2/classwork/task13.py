40b2762079fd1e1f59e05b0592982c045b29fc67
dmitr <dmitry.yurackov@yandex.ru>
2023-04-12 19:48:51 +0300

    Сделал 13

--- UNIT_2/classwork/task13.py
+++ UNIT_2/classwork/task13.py
@@ -1,7 +1,25 @@
 # todo:  Ввести число n. Напечатать треугольник из символов +.
 #  Пример для n = 5:
+'''
 +
 ++
 +++
 ++++
 +++++
+'''
+
+
+n = 6
+
+def treugolnic(chislo):
+    e='*'
+    for i in range(chislo):
+        if i == 0:
+            print(e)
+        else:
+            e = e + '*'
+            print(e)
+
+treugolnic(n)
+
+
