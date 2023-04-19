323f2cd66687a5f53909376fe02e49c909e31f80
dmitr <dmitry.yurackov@yandex.ru>
2023-04-19 18:27:28 +0300

    Добавляю 4 занятие

---
+++ UNIT_4/classwork/task35.py
@@ -0,0 +1,4 @@
+#todo: Заданы два списка. Необходимо их сериализовать в один файл.
+
+list_one = [True, 'If the implementation is hard to explain, it\'s a bad idea.', {'age': 27}]
+list_two = [False, 'Sparse is better than dense.', {'age': 90}]
